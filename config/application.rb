aa18054d138610319120d95d2fc0e6cec0b34dc4
eiz <sashkaseb@mail.ru>
2023-09-20 10:35:51 +0300

    правки #3

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,6 @@ Bundler.require(*Rails.groups)
 module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-    config.time_zone = 'Moscow'
-    config.i18n.default_locale = :ru
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/initializers/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Testguru
-  class Application
-    VERSION = '1.0.0'
-  end
-end
