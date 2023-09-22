14d57934371b165a2487cd2e151cd347a2c4ad33
eiz <sashkaseb@mail.ru>
2023-09-22 16:09:02 +0300

    Поправил миграционные файлы

--- db/migrate/20230921170802_create_tests.rb
+++ db/migrate/20230921170802_create_tests.rb
@@ -1,9 +1,10 @@
 class CreateTests < ActiveRecord::Migration[7.0]
   def change
     create_table :tests do |t|
-      t.string :title
-      t.integer :level
-
+      t.string :title, null: false
+      t.integer :level, null: false, default: false
+      t.references :category, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
