88791a3e15ee7af5708d25c915ef2bc2e82b8d86
eiz <sashkaseb@mail.ru>
2023-09-24 17:03:22 +0300

    Создал сиды и написал методы для моделей

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  def self.tests_by_category(category_title)
+    joins(:category).where(category: { title: category_title }).order(id: :desc).pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+answers = [
+  { body: 'answer_1', correct: false, question_id: 1 },
+  { body: 'answer_2', correct: false, question_id: 3 },
+  { body: 'answer_3', correct: false, question_id: 1 }
+]
+categories = [
+  { title: 'category_1' },
+  { title: 'category_2' },
+  { title: 'category_3' }
+]
+questions = [
+  { body: 'question_1', test_id: 2 },
+  { body: 'question_2', test_id: 1 },
+  { body: 'question_3', test_id: 3 }
+]
+tests = [
+  { title: 'test_1', level: 2, category_id: 1, user_id: 3 },
+  { title: 'test_2', level: 3, category_id: 2, user_id: 3 },
+  { title: 'test_3', level: 3, category_id: 1, user_id: 1 }
+]
+users = [
+  { name: 'Name_1' },
+  { name: 'Name_2' },
+  { name: 'Name_3' }
+]
+
+users.each { |line| User.create(line) }
+categories.each { |line| Category.create(line) }
+tests.each { |line| Test.create(line) }
+questions.each { |line| Question.create(line) }
+answers.each { |line| Answer.create(line) }
