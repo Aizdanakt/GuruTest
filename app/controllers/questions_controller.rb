0a438aea9916b8762303aa4da33f2a51079fc96f
eiz <sashkaseb@mail.ru>
2023-10-08 21:21:42 +0300

    Дописал контроллер questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,28 +1,44 @@
 class QuestionsController < ApplicationController
-  before_action :find_question, only: %i[show]
+  before_action :find_test, only: %i[index new create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_questions_not_found
 
   def index
-    render plain: { questions: Question.where(test_id: params[:test_id]) }
+    @questions = @test.questions
+    render plain: { questions: @questions }
   end
 
   def show
+    @question = Question.find(params[:id])
     render inline: '<%= @question.body %>'
   end
 
-  def new; end
-
-  def create
-    render plain: { question: params[:question] }
+  def new
+    @question = Question.new
   end
 
-  def delete; end
+  def create
+    @question = @test.questions.build question_params
+    if @question.save
+      render plain: 'success'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question.destroy
+    render plain: 'success'
+  end
 
   private
 
-  def find_question
-    @question = Question.find_by!(id: params[:id], test_id: params[:test_id])
+  def question_params
+    params.require(:question).permit(:body, :test_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
   end
 
   def rescue_with_questions_not_found
