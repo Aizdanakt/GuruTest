692cbecc92a9773b171f0615e4e17558c7b8965f
eiz <sashkaseb@mail.ru>
2023-10-28 12:54:09 +0300

    Доделал стилизацию приложения и улучшил дизайн flash сообщений

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -2,7 +2,9 @@ module FlashHelper
 
   def display_flash_messages
     flash.map do |type, message|
-      content_tag :p, message, class: "flash #{type}"
+      content_tag :div, class: "alert alert-#{type} text-white border border-primary", role: 'alert' do
+        concat message
+      end
     end.join.html_safe
   end
 end
