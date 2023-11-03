5e28407555e18ad48f1626b55ffff13a7fd79319
eiz <sashkaseb@mail.ru>
2023-11-03 16:58:08 +0300

    add javascript functional

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "controllers"
 import "@hotwired/turbo-rails"
+import "./controllers"
 import "./utilities/sorting"
-Turbolinks.start()
+import "./utilities/password_confirm"
---
+++ app/javascript/utilities/password_confirm.js
@@ -0,0 +1,20 @@
+const passwordInput = document.getElementById('user_password');
+const confirmPasswordInput = document.getElementById('user_password_confirmation');
+const matchIcon = document.getElementById('password-match-icon');
+
+if (passwordInput && confirmPasswordInput) {
+    confirmPasswordInput.addEventListener('input', function() {
+        const password = passwordInput.value;
+        const confirmPassword = confirmPasswordInput.value;
+
+        if (password === confirmPassword && confirmPassword !== '') {
+            matchIcon.innerHTML = '✔️';
+            matchIcon.style.color = 'green';
+        } else if (confirmPassword === '') {
+            matchIcon.innerHTML = '';
+        } else {
+            matchIcon.innerHTML = '❌';
+            matchIcon.style.color = 'red';
+        }
+    });
+}
