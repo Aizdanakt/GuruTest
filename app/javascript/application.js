4a7d6c74c2b4664e6a62707eca12a9284af77d55
eiz <sashkaseb@mail.ru>
2023-11-08 22:22:03 +0300

    fix static files and js controllers

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "./utilities/sorting"
-import "./utilities/password_confirm"
-import "./utilities/form_inline"
-import "./utilities/progres_bar"
-import "./controllers"
+import "controllers"
