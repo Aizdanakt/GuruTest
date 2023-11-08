6a2c3cdbf831bd5464f5cf65244042aec16e96a0
eiz <sashkaseb@mail.ru>
2023-11-08 13:47:07 +0300

    fix js files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "../assets/builds/utilities/sorting"
-import "../assets/builds/utilities/password_confirm"
-import "../assets/builds/utilities/form_inline"
-import "../assets/builds/utilities/progres_bar"
+import "./utilities/sorting"
+import "./utilities/password_confirm"
+import "./utilities/form_inline"
+import "./utilities/progres_bar"
 import "controllers"
