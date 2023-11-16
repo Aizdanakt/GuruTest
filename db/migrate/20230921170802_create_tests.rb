a4caba07a284a764f7857fbaebe240ad87e46bdc
eiz <sashkaseb@mail.ru>
2023-11-16 19:53:33 +0300

    fix schema

--- db/migrate/20230921170802_create_tests.rb
+++ db/migrate/20230921170802_create_tests.rb
@@ -3,7 +3,7 @@ class CreateTests < ActiveRecord::Migration[7.0]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, null: false, default: false
-      t.references :category, null: false, foreign_key: true
+      t.references :category, foreign_key: true
       t.references :author, null: false, foreign_key: { to_table: :users }
       t.timestamps
     end
